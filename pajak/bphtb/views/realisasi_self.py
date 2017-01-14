09f6e157d01103f28abc80f350aa375cebe6b236
aagusti <aa.gustiana@gmail.com>
2017-01-14 13:38:42 +0700

    Update Aris 14012017

--- pajak/bphtb/views/realisasi_self.py
+++ pajak/bphtb/views/realisasi_self.py
@@ -118,8 +118,38 @@ class BphtbViewSelf(BphtbView):
                        SspdBphtb.kode=='1')
         url_dict = self.req.matchdict
         if url_dict['rpt']=='csv' :
-            filename = 'saldo_awal.csv'
+            filename = 'bphtb_realisasi_self.csv'
             return csv_response(self.req, csv_rows(query), filename)
+
+        if url_dict['rpt']=='pdf' :
+            _here = os.path.dirname(__file__)
+            path = os.path.join(os.path.dirname(_here), 'static')
+            print "XXXXXXXXXXXXXXXXXXX", os.path
+
+            #logo = path + "/img/logo.png"
+            #line = path + "/img/line.png"
+            #logo = self.req.static_url('static/img/logo.png')
+            #line = self.req.static_url('static/img/line.png')
+            logo = "http://192.168.56.3:6543/static/img/logo.png"
+            line = "http://192.168.56.3:6543/static/img/line.png"
+
+            path = os.path.join(os.path.dirname(_here), 'reports')
+            rml_row = open_rml_row(path+'/bphtb_realisasi_self.row.rml')
+            
+            rows=[]
+            for r in query.all():
+                s = rml_row.format(nop=r.nop, tahun=r.tahun, ke=r.ke, wp_nama=r.wp_nama, pokok=r.pokok, denda=r.denda,   
+                                   bayar=r.bayar, posted=r.posted)
+                rows.append(s)
+            
+            pdf, filename = open_rml_pdf(path+'/bphtb_realisasi_self.rml', rows=rows, 
+                                company=self.req.company,
+                                departement = self.req.departement,
+                                logo = logo,
+                                line = line,
+                                address = self.req.address)
+            return pdf_response(self.req, pdf, filename)
+
 
     ###########
     # Posting #
